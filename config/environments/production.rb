3dfd6ea24299785f21a5bce5637c62fdad713b53
renjingjing <renjingjing@gmail.com>
2015-11-10 11:15:42 -0800

    p1.1_practice_heroku_download_ysacamp_assets_backgroundimage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # for heroku add
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
